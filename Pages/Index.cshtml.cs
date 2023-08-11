96b243b9bc9ea1a2cad241ec55669ebdab2f9b3b
James Adam <69864846+James-Adam@users.noreply.github.com>
2023-08-11 11:12:19 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using FinanceNewsTicker.Models;
+using FinanceNewsTicker.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceNewsTicker.Pages;
+
+public class IndexModel : PageModel
+{
+    //reference
+    private readonly ILogger<IndexModel> _logger;
+
+    private readonly INewsService _newsService;
+    public FinanceNews news;
+
+    public IndexModel(ILogger<IndexModel> logger,
+        INewsService newsService)
+    {
+        _logger = logger;
+        _newsService = newsService;
+    }
+
+    public void OnGet()
+    {
+        news = _newsService.GetFinanceNews(0); //no offset
+    }
+
+    public void OnGetLoadMoreNews(int offset)
+    {
+        news = _newsService.GetFinanceNews(offset);
+    }
+}
---
+++ Services/NewsService.cs
@@ -0,0 +1,47 @@
+﻿using FinanceNewsTicker.Models;
+using Newtonsoft.Json;
+
+namespace FinanceNewsTicker.Services;
+
+public class NewsService : INewsService
+{
+    //dependency injection
+    private readonly IConfiguration _configuration;
+
+    //constructor
+    public NewsService(IConfiguration configuration) //DI
+    {
+        _configuration = configuration; //DI
+    }
+
+    public FinanceNews GetFinanceNews(int offset)
+    {
+        var apiKey = _configuration.GetValue<string>("API_KEY");
+        var baseUrl = _configuration.GetValue<string>("API_URL");
+
+        //HttpClient request to API
+
+        using HttpClient client = new();
+        client.BaseAddress = new Uri(baseUrl!);
+
+        var parameters =
+            string.Format("?apikey={0}&offset={1}&date={2}&sort={3}", apiKey, offset, "today", "desc");
+
+        //HTTP response
+        var response = client.GetAsync(parameters).Result;
+
+        //check to see if response works
+        if (response.IsSuccessStatusCode)
+        {
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<FinanceNews>(result);
+        }
+
+        return new FinanceNews
+        {
+            Data = new List<NewsArticle>(),
+            Pagination = new Pagination()
+        };
+    }
+}
